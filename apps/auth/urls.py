4fe334919b008c1789497b3af02c7523db1d20a9
MoMoskar <aagamanmainali35@gmail.com>
2026-09-02 02:51:56 +0545

    refcator: renamed the register api to sync employee api

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
+from drf_spectacular.utils import extend_schema
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.auth.views import (
     ChangePasswordView,
     CurrentUser,
     LoginView,
-    RegisterView,
 )
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path("register/", RegisterView.as_view(), name="register"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("me/", CurrentUser.as_view(), name="me"),
     path("change-password/", ChangePasswordView.as_view(), name="change-password"),
