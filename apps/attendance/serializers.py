7e7dfc6a934c48f196c5e3dbdbfe4ec0083838e4
MoMoskar <aagamanmainali35@gmail.com>
2026-09-02 10:31:26 +0545

    Feat: Added the serialier for sync state,device,DailyAttendnace and Device attendnace models

--- apps/attendance/serializers.py
+++ apps/attendance/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
 
-from .models import Device
+from apps.attendance.models import (
+    DailyAttendanceLog,
+    Device,
+    DeviceAttendanceLog,
+    SyncState,
+)
+
+
+class SyncStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SyncState
+        fields = [
+            "device_serial",
+            "last_synced_at",
+            "last_run_at",
+        ]
 
 
 class DeviceSerializer(serializers.ModelSerializer):
@@ -14,3 +29,35 @@ class DeviceSerializer(serializers.ModelSerializer):
             "is_active",
         ]
         read_only_fields = ["id"]
+
+
+class DeviceSyncStateSerializer(serializers.Serializer):
+    serial = serializers.CharField()
+    name = serializers.CharField()
+    ip_address = serializers.CharField()
+    port = serializers.IntegerField()
+    is_active = serializers.BooleanField()
+    last_synced_at = serializers.DateTimeField(allow_null=True)
+    last_run_at = serializers.DateTimeField(allow_null=True)
+
+
+class DailyAttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyAttendanceLog
+        fields = [
+            "employee",
+            "date",
+            "check_in_time",
+            "check_out_time",
+            "total_hours",
+            "status",
+            "is_late",
+            "is_early_leave",
+            "early_leave_minutes",
+        ]
+
+
+class RawAttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeviceAttendanceLog
+        fields = "__all__"
