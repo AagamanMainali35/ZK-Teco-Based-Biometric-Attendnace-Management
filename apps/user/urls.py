8003150a878ebc73e90484380b2cd302b3fbab16
MoMoskar <aagamanmainali35@gmail.com>
2026-09-04 15:40:15 +0545

    Feat: Added my-attendance route to user endpoints

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from apps.user.views import SyncEmployeeView
+
+from apps.user.views import SyncEmployeeView , EmployeeListView
 
 urlpatterns = [
     path("sync-employee/", SyncEmployeeView.as_view(), name="sync-employee"),
+    path("my-attendance/", EmployeeListView.as_view(), name="employee-attendance"),
+
 ]
