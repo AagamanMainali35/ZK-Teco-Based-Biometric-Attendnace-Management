0f3dd6bb14812b67626c8be3dbb356d1bf1de7cf
MoMoskar <aagamanmainali35@gmail.com>
2026-09-05 10:48:10 +0545

    fix(attendance): standardize policy router basename

--- apps/attendance/urls.py
+++ apps/attendance/urls.py
@@ -5,17 +5,17 @@ from apps.attendance.views import (
     DailyAttendanceViewSet,
     DeviceSyncStateView,
     DeviceView,
+    PolicyView,
     PullAttendanceView,
     RawAttendanceView,
     TestConnectionView,
-    PolicyView
 )
 
 router = DefaultRouter()
 router.register("device", DeviceView, basename="device")
 router.register("attendance-log", DailyAttendanceViewSet, basename="attendance-log")
 router.register("raw/attendance-log", RawAttendanceView, basename="raw-attendance-log")
-router.register("policy", PolicyView,basename="PolicyView")
+router.register("policy", PolicyView, basename="policy")
 
 
 urlpatterns = [
