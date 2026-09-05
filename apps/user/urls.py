2fb732b031cc592fc5327d45809f8b1050f9ad2d
MoMoskar <aagamanmainali35@gmail.com>
2026-09-05 14:12:05 +0545

    fix(user): standardize user router naming and clean department routes

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,17 +2,16 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from apps.user.views import (
-    EmployeeListView,
+    EmployeeViewSet,
     GroupsView,
     PermissionView,
-    SyncEmployeeView,
 )
 
-routers = DefaultRouter()
-routers.register("permission", PermissionView, basename="permissions")
-routers.register("groups", GroupsView, basename="groups")
+router = DefaultRouter()
+router.register("employees", EmployeeViewSet, basename="employees")
+router.register("permissions", PermissionView, basename="permissions")
+router.register("groups", GroupsView, basename="groups")
+
 urlpatterns = [
-    path("sync-employee/", SyncEmployeeView.as_view(), name="sync-employee"),
-    path("my-attendance/", EmployeeListView.as_view(), name="employee-attendance"),
-    path("", include(routers.urls)),
+    path("", include(router.urls)),
 ]
